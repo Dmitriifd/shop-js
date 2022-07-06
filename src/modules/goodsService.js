997e7dc23413dad06144175a2ecdf8451a04d324
Dmitriifd <idimka9@gmail.com>
2022-07-06 22:03:56 +0500

    работа с api

--- src/modules/goodsService.js
+++ src/modules/goodsService.js
@@ -1,6 +1,8 @@
-import { API_URL } from "./var";
+import { API_URL } from './var';
 
-export const getGoods = ({page}) => {
-    const url = new URL(`${API_URL}api/goods`);
-    fetch(url);
-}
+export const getGoods = ({ page }) => {
+	const url = new URL(`${API_URL}api/goods`);
+	if (page) url.searchParams.append('page', page);
+
+	return fetch(url).then((response) => response.json())
+};
